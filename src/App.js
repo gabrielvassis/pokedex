88d503aa7f8ee36eba98e53ffa5fe49471d541c5
Gabriel Assis <biel.v.assis@gmail.com>
2023-10-16 20:51:25 -0300

    api connection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div>OI </div>;
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchInfo = async () => {
+    const response = await axios.get(url);
+    setPokemons(response.data.results);
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return (
+    <div>
+      {pokemons.map((pokemon, index) => {
+        return (
+          <div key={index}>
+            <p style={{ fontSize: 20, color: "red" }}>{pokemon.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
