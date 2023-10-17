34d55dd17e8b8305c611b4ada34aee684f38872c
Gabriel Assis <biel.v.assis@gmail.com>
2023-10-16 21:14:16 -0300

    showing pokemon sprites

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 function App() {
-  const url = "https://pokeapi.co/api/v2/pokemon/";
+  const url = "https://pokeapi.co/api/v2/pokemon/?limit=151";
   const [pokemons, setPokemons] = useState([]);
 
   const fetchInfo = async () => {
     const response = await axios.get(url);
-    setPokemons(response.data.results);
+    const rawPokemons = response.data.results;
+    for (let i = 0; i < rawPokemons.length; i++) {
+      const pokemonData = await axios.get(rawPokemons[i].url);
+      rawPokemons[i].sprite = pokemonData.data.sprites.front_default;
+    }
+    setPokemons(rawPokemons);
   };
 
   useEffect(() => {
@@ -20,7 +25,8 @@ function App() {
       {pokemons.map((pokemon, index) => {
         return (
           <div key={index}>
-            <p style={{ fontSize: 20, color: "red" }}>{pokemon.name}</p>
+            <img src={pokemon.sprite}></img>
+            {pokemon.name}
           </div>
         );
       })}
