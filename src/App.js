cfdde0592a33ca3218f50b4101fe9ce2e1b25384
Gabriel Assis <biel.v.assis@gmail.com>
2023-10-18 20:18:41 -0300

    added load more button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Card, Button } from "antd";
 
 function App() {
-  const url = "https://pokeapi.co/api/v2/pokemon/";
+  const url = "https://pokeapi.co/api/v2/pokemon/?limit=1";
   const [pokemons, setPokemons] = useState([]);
+  const [nextPokemonsToLoad, setNextPokemonsToLoad] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const fetchInfo = async () => {
     const response = await axios.get(url);
+    setNextPokemonsToLoad(response.data.next);
     const rawPokemons = response.data.results;
     await Promise.all(
       rawPokemons.map(async (pokemon) => {
@@ -18,18 +22,39 @@ function App() {
     setPokemons(rawPokemons);
   };
 
+  const loadMorePokemons = async () => {
+    setLoading(true);
+    const response = await axios.get(nextPokemonsToLoad);
+    setNextPokemonsToLoad(response.data.next);
+
+    const addPokemons = response.data.results;
+    await Promise.all(
+      addPokemons.map(async (pokemon) => {
+        const pokemonData = await axios.get(pokemon.url);
+        pokemon.sprite = pokemonData.data.sprites.front_default;
+      })
+    );
+    addPokemons.forEach((current) => {
+      pokemons.push(current);
+    });
+    setPokemons(pokemons);
+    setLoading(false);
+  };
+
   useEffect(() => {
     fetchInfo();
   }, []);
 
   return (
     <div>
+      <Button type="primary" loading={loading} onClick={loadMorePokemons}>
+        Load more
+      </Button>
       {pokemons.map((pokemon, index) => {
         return (
-          <div key={index}>
+          <Card key={index} title={pokemon.name} style={{ width: 300 }}>
             <img src={pokemon.sprite} alt={`Sprite for ${pokemon.name}`}></img>
-            {pokemon.name}
-          </div>
+          </Card>
         );
       })}
     </div>
