f7fbbe74046aecd83020b16e12a08ac7e66cfb7e
Gabriel Assis <biel.v.assis@gmail.com>
2023-10-18 21:15:09 -0300

    added new component

--- src/App.js
+++ src/App.js
@@ -1,64 +1,7 @@
 import "./App.css";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { Card, Button } from "antd";
-
-function App() {
-  const url = "https://pokeapi.co/api/v2/pokemon/?limit=1";
-  const [pokemons, setPokemons] = useState([]);
-  const [nextPokemonsToLoad, setNextPokemonsToLoad] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const fetchInfo = async () => {
-    const response = await axios.get(url);
-    setNextPokemonsToLoad(response.data.next);
-    const rawPokemons = response.data.results;
-    await Promise.all(
-      rawPokemons.map(async (pokemon) => {
-        const pokemonData = await axios.get(pokemon.url);
-        pokemon.sprite = pokemonData.data.sprites.front_default;
-      })
-    );
-    setPokemons(rawPokemons);
-  };
-
-  const loadMorePokemons = async () => {
-    setLoading(true);
-    const response = await axios.get(nextPokemonsToLoad);
-    setNextPokemonsToLoad(response.data.next);
-
-    const addPokemons = response.data.results;
-    await Promise.all(
-      addPokemons.map(async (pokemon) => {
-        const pokemonData = await axios.get(pokemon.url);
-        pokemon.sprite = pokemonData.data.sprites.front_default;
-      })
-    );
-    addPokemons.forEach((current) => {
-      pokemons.push(current);
-    });
-    setPokemons(pokemons);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchInfo();
-  }, []);
-
-  return (
-    <div>
-      <Button type="primary" loading={loading} onClick={loadMorePokemons}>
-        Load more
-      </Button>
-      {pokemons.map((pokemon, index) => {
-        return (
-          <Card key={index} title={pokemon.name} style={{ width: 300 }}>
-            <img src={pokemon.sprite} alt={`Sprite for ${pokemon.name}`}></img>
-          </Card>
-        );
-      })}
-    </div>
-  );
-}
+import PokemonList from "./components/PokemonList.js";
+const App = () => {
+  return <PokemonList />;
+};
 
 export default App;
---
+++ src/components/PokemonList.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Card, Button } from "antd";
+import ScrollableAnchor from "react-scrollable-anchor";
+
+const PokemonList = () => {
+  const url = "https://pokeapi.co/api/v2/pokemon/?limit=1";
+  const [pokemons, setPokemons] = useState([]);
+  const [nextPokemonsToLoad, setNextPokemonsToLoad] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchInfo = async () => {
+    const response = await axios.get(url);
+    setNextPokemonsToLoad(response.data.next);
+    const rawPokemons = response.data.results;
+    await Promise.all(
+      rawPokemons.map(async (pokemon) => {
+        const pokemonData = await axios.get(pokemon.url);
+        pokemon.sprite = pokemonData.data.sprites.front_default;
+      })
+    );
+    setPokemons(rawPokemons);
+  };
+
+  const loadMorePokemons = async () => {
+    setLoading(true);
+    const response = await axios.get(nextPokemonsToLoad);
+    setNextPokemonsToLoad(response.data.next);
+
+    const addPokemons = response.data.results;
+    await Promise.all(
+      addPokemons.map(async (pokemon) => {
+        const pokemonData = await axios.get(pokemon.url);
+        pokemon.sprite = pokemonData.data.sprites.front_default;
+      })
+    );
+    addPokemons.forEach((current) => {
+      pokemons.push(current);
+    });
+    setPokemons(pokemons);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return (
+    <div>
+      <Button
+        type="primary"
+        loading={loading}
+        onClick={loadMorePokemons}
+        style={{ width: 300, position: "fixed", zIndex: 1 }}
+      >
+        Load more
+      </Button>
+      <ScrollableAnchor>
+        <div style={{ paddingTop: 15 }}>
+          {pokemons.map((pokemon, index) => {
+            return (
+              <Card key={index} title={pokemon.name} style={{ width: 300 }}>
+                <img
+                  src={pokemon.sprite}
+                  alt={`Sprite for ${pokemon.name}`}
+                ></img>
+              </Card>
+            );
+          })}
+        </div>
+      </ScrollableAnchor>
+    </div>
+  );
+};
+
+export default PokemonList;
